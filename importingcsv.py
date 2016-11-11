b43d0b42f170eebf231451e386d431d977fcbae5
bujak <bujakowski@gmail.com>
2016-11-11 13:10:19 +0100

    upgrade in import function, comments

--- importingcsv.py
+++ importingcsv.py
@@ -7,18 +7,25 @@ inv = {'rope': 1,
        'arrow': 12}
 
 
-def import_inventory(inventory, filename):
+def import_inventory(inventory, filename="import_inventory.csv"):
     with open(filename, 'r') as saved_inv:
         for line in saved_inv:
-            x = line.split(",")
-            a = x[0]
-            b = x[1]
-            c = len(b)-1
-            b = b[0:c]
-            inventory[a] = int(b)
-        """print(inv)"""
+            line = line.split(",")  # putting line from csv file to list
+            item = line[0]  #initalizing items in list as variables
+            count = line[1]
+            count_len = len(count) - 1
+            count = count[0:count_len]  #cutting \n from count string
+            importing={}
+            importing[item] = int(count)  #putting items and count in temporary dict.
+            for item in importing:  #adding items to main inventory
+                if item not in inventory:
+                    inventory[item] = int(count)
+                else:
+                    inventory[item] += int(count)
+        #print(inv) #just for control
 
 
+# printing unsorted inventory with for loop
 def display_inventory(inventory):
     print('\nInventory:')
     for key, value in inventory.items():
@@ -26,6 +33,7 @@ def display_inventory(inventory):
     print('Total items :', sum(inventory.values()))
 
 
+# adding items from loot list to inventory
 def add_to_inventory(inventory, items):
     for item in items:
         if item in inventory:
@@ -34,6 +42,7 @@ def add_to_inventory(inventory, items):
             inventory[item] = 1
 
 
+#saving inventory to external file
 def export_inventory(inventory, filename=None):
     if filename is None:
         with open('export_inventory.csv', 'w') as f:
@@ -47,9 +56,10 @@ def export_inventory(inventory, filename=None):
                 writer.writerow(row)
 
 
+#print inventory, sorted or not, depending on second parameter
 def print_table(inventory, order=None):
     print("\nInventory:")
-    print("{:>7} {:>12} {:>0}".format('count', 'item name','\n--------------------'))
+    print("{:>7} {:>12} {:>0}".format('count', 'item name', '\n--------------------'))
     if order == 'count,desc':
         for key, value in sorted(inventory.items(), key=lambda x: x[1], reverse=True):
             print("{:>7} {:>12}".format(value, key))
@@ -61,17 +71,19 @@ def print_table(inventory, order=None):
             print("{:>7} {:>12}".format(value, key))
     print('--------------------\nTotal items :', sum(inventory.values()))
 
-#
-# import_inventory(inv, 'inventory.csv')
-#
-# display_inventory(inv)
-#
-# loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-#
-# add_to_inventory(inv, loot)
-#
-# display_inventory(inv)
-#
-# export_inventory(inv, 'rubu')
 
-print_table(inv)
+if __name__ == '__main__':
+
+    import_inventory(inv)
+
+    display_inventory(inv)
+
+    loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+    add_to_inventory(inv, loot)
+
+    display_inventory(inv)
+
+    export_inventory(inv, 'rubu')
+
+    print_table(inv)
